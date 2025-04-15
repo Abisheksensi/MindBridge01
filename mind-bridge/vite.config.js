330567132d01cba5636ee419aad8424e334141f8
Abisheksensi <jayathilakeabishek@gmail.com>
2025-04-16 02:06:30 +0530

    Refactor Vite configuration for improved readability and formatting

--- mind-bridge/vite.config.js
+++ mind-bridge/vite.config.js
@@ -2,15 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
+    react()
+  ],
 
-    build: {
+  build: {
     outDir: 'dist' // This must match the publish directory
+  },
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
