dd0e13d4ea63cddd55dc8b3d3567bce6a2a4375c
Abisheksensi <jayathilakeabishek@gmail.com>
2025-04-16 09:40:38 +0530

    Refactor Vite configuration for improved structure and clarity

--- mind-bridge/vite.config.js
+++ mind-bridge/vite.config.js
@@ -2,17 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
-
   build: {
-    outDir: 'dist' // This must match the publish directory
+    outDir: 'dist',
+    assetsDir: 'assets', // Output assets to dist/assets/
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/main-[hash].js', // e.g., assets/main-CkN0H0W1.js
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   },
-
   server: {
     proxy: {
       "/api": {
